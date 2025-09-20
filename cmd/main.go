b1ccf1b021e5e2042afa702a5b4b005df580c3cb
Mu'ti C Putro <mcputro@Mutis-MacBook-Pro.local>
2025-09-20 18:28:56 +0700

    add retry 5 times for create connection database

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	"github.com/MCPutro/go-management-project/internal/config"
 	"github.com/MCPutro/go-management-project/internal/config/database"
 	"github.com/MCPutro/go-management-project/internal/delivery/handler"
@@ -8,7 +10,6 @@ import (
 	"github.com/MCPutro/go-management-project/internal/repository"
 	"github.com/MCPutro/go-management-project/internal/usecase"
 	"github.com/gofiber/fiber/v2"
-	"log"
 )
 
 func main1() {
@@ -38,14 +39,14 @@ func main1() {
 }
 
 func main() {
-	config, err := config.LoadConfig()
+	loadConfig, err := config.LoadConfig()
 	if err != nil {
-		log.Fatalln("1- ", err)
+		log.Fatalln("failed to load config:", err)
 	}
 
-	postgresDb, err := database.NewPostgresDB(config.GetDatabaseConfig())
+	postgresDb, err := database.NewPostgresDB(loadConfig.GetDatabaseConfig())
 	if err != nil {
-		log.Fatalln("2- ", err)
+		log.Fatalln("failed to connect to database:", err)
 	}
 	defer postgresDb.Close()
 
@@ -57,8 +58,8 @@ func main() {
 
 	router.RegisterUserRoutes(app, userHandler)
 
-	err = app.Listen(":" + config.GetApplicationConfig().Port)
+	err = app.Listen(":" + loadConfig.GetApplicationConfig().Port)
 	if err != nil {
-		log.Fatalln("3- ", err)
+		log.Fatalln("failed to listen:", err)
 	}
 }
--- internal/config/database/db.go
+++ internal/config/database/db.go
@@ -2,26 +2,41 @@ package database
 
 import (
 	"database/sql"
-	"fmt"
+	"errors"
+	"log"
+	"time"
+
 	"github.com/MCPutro/go-management-project/internal/config"
 	_ "github.com/lib/pq"
-	"time"
 )
 
 func NewPostgresDB(config *config.DatabaseConfig) (*sql.DB, error) {
+	var db *sql.DB
+	var err error
 
-	db, err := sql.Open(config.PostgresSql.Name, config.PostgresSql.DSN)
-	if err != nil {
-		return nil, err
+	for i := 0; i < 5; i++ {
+		db, err = sql.Open(config.PostgresSql.Name, config.PostgresSql.DSN)
+		if err != nil {
+			return nil, err
+		}
+
+		// test connection
+		err = db.Ping()
+		if err != nil {
+			log.Println("failed to connect to database, retry in 5 seconds")
+			time.Sleep(5 * time.Second)
+		} else {
+			db.SetMaxIdleConns(5)
+			db.SetMaxOpenConns(100)
+			db.SetConnMaxLifetime(60 * time.Minute)
+			db.SetConnMaxIdleTime(10 * time.Minute)
+
+			log.Println("successfully connected to database")
+
+			return db, nil
+		}
 	}
 
-	db.SetMaxIdleConns(5)
-	db.SetMaxOpenConns(100)
-	db.SetConnMaxLifetime(60 * time.Minute)
-	db.SetConnMaxIdleTime(10 * time.Minute)
-
-	fmt.Println("Database connected successfully")
-
-	return db, nil
+	return nil, errors.New("failed to connect to database, please check your database configuration")
 
 }
